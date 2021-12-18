ce79a6e2bce492e41ea7c3373ce735c2f4da1cb9
Fran <franamezcuamedina@gmail.com>
2021-12-18 15:24:51 +0100

    endpoints of CRUD finish

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tuits')
+// if use @UseGuards(AuthGuard('jwt')) then the user must be logged in
+// commented in development
+// @UseGuards(AuthGuard('jwt'))
 export class TuitsController {
 
-
-    @Get('/')
+    @Get()
     getTuits() {
         return 'Tuits';
     }
 
+    @Get(':id')
+    getTuit(@Param('id') id) {
+        console.log(id);
+        return `Tuit ${id}`;
+    }
+
+    @Post('create')
+    createTuit(@Body('message') message) {
+        return 'Tuit created: ' + message;
+    }
+
+    @Patch(':id')
+    updateTuit(@Param('id') id: string, @Body('tuit') tuit: string) {
+        return 'Tuit updated: ' + id + ' con contenido ' + tuit;
+    }
+
+    @Delete(':id')
+    deleteTuit(@Param('id') id: string) {
+        return 'Tuit deleted: ' + id;
+    }
 
 }
